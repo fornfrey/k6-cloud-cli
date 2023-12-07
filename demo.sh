b1c1919c202edd867f90d3ef0eaf02ae4b100353
Llandy Riveron Del Risco <llandy3d@gmail.com>
2023-12-07 12:57:22 +0100

    demo validation

--- demo.sh
+++ demo.sh
@@ -8,6 +8,38 @@ export K6_CLOUD_TOKEN=user-token;
 export K6_CLOUD_HOST=http://localhost:9875;
 export K6_CLOUD_API_HOST=http://api.dev.k6.io
 
+# script initialization for script validation test
+echo "
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 10,
+    duration: '30s'
+};
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
+" > correct-script.js
+
+echo "
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 10,
+    duration: '30s',
+	wrong
+};
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
+" > wrong-script.js
+
 ./k6 cloud loadzone list
 
 ./k6 cloud organization list
@@ -26,3 +58,13 @@ export K6_CLOUD_API_HOST=http://api.dev.k6.io
 ./k6 cloud testrun download 1
 ./k6 cloud testrun get 1
 ./k6 cloud testrun list 1
+
+./k6 cloud validate correct-script.js
+
+# this needs to fail so we don't exit the script
+set +e
+./k6 cloud validate wrong-script.js
+
+# cleanup
+rm correct-script.js
+rm wrong-script.js
